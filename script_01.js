b6e623284a14ade939ee8e069a866a647099145c
Hindax <haar162@more2learn.de>
2022-11-17 13:00:52 +0100

    objekt hierarchie

--- script_01.js
+++ script_01.js
@@ -26,9 +26,52 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person =   {
 
+        firstName:"Hind",
+
+        familyName: "Aarif" ,  
+
+        salary :[120000,160000],
+
+        permission: true,  //To check again
+
+        sayHello:
+        function ()
+         { return "Hello, ich bin " + this.firstName; }
+
+}
+     
+// output(person);
+output(person.firstName);
+output(person.familyName);
+
+const txt =     "ich bin " + person.firstName + " und verdiene " + 
+                person.salary[1] + "p.a";
+
+output(txt);
+output(person.sayHello);
+
+// output(person.firstName + person.familyName);
 
 /** Ausgabe */
 function output(outputData) {
         console.log(outputData);
+
 }
+
+// Objekt 2
+
+const baikal = {
+                value:"10m",
+                deep: {
+                        deeper:{
+                                deepest:  "Das Licht - auf 1642m!"
+                        }
+                }
+                }
+/**Ausgabe */
+output(baikal.value);
+output(baikal.deep.deeper.deepest); // dot syntax
+
+//192.168.1.0  // apple.com
